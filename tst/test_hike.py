63b879f5883334c26a73173d1117baa03449bfa2
Isaiah Mann <isaiahbmann@gmail.com>
2019-06-02 17:29:31 -0700

    Add hike data object

---
+++ tst/test_hike.py
@@ -0,0 +1,8 @@
+from src.hike import Hike
+
+
+def test_create_hike():
+    hike = Hike("Nice Trail", 100.2221, 20.00, 200, 5, ["forest", "waterfall"], ["icon"], True)
+    print(hike)
+
+test_create_hike()
