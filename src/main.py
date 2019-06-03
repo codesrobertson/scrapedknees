e2580b43c266fda986631526b789a36cd3d8ec21
codesrobertson <arobertson335@gmail.com>
2019-06-02 23:07:36 -0700

    Began scraping WTA index for content info.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 from hike import Hike
 
-print("Hello World")
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
+hike = Hike("Nice Trail", 100.2221, 20.00, 200, 5, ["forest", "waterfall"], ["icon"], True)
+print(hike)
+
+class ScrapedIndex:
+    index_link = "https://www.wta.org/go-outside/hikes"
+    index_response = requests.get(index_link)
+    print (index_response)
+    index_content = BeautifulSoup (index_response.content, "html.parser")
+    div = index_content.find(id="search-result-listing")
+    for hike_div in div.findChildren ("div"):
+        print(hike_div)
+
+print(ScrapedIndex())
