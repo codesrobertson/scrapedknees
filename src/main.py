4af2308bbf1898e7efef24c8371033fdffa233ea
codesrobertson <arobertson335@gmail.com>
2019-10-02 22:24:15 -0700

    Created hikes from webpage.

--- src/main.py
+++ src/main.py
@@ -2,16 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 from hike import Hike
 
-hike = Hike("Nice Trail", 100.2221, 20.00, 200, 5, ["forest", "waterfall"], ["icon"], True)
-print(hike)
-
 class ScrapedIndex:
-    index_link = "https://www.wta.org/go-outside/hikes"
-    index_response = requests.get(index_link)
-    print (index_response)
-    index_content = BeautifulSoup (index_response.content, "html.parser")
-    div = index_content.find(id="search-result-listing")
-    for hike_div in div.findChildren ("div"):
-        print(hike_div)
+    def all_hikes(self):
+        firp_hikes = []
+        index_link = "https://www.wta.org/go-outside/hikes"
+        index_response = requests.get(index_link)
+        index_content = BeautifulSoup (index_response.content, "html.parser")
+        div = index_content.find(id="search-result-listing")
+        for hike_div in div.findChildren ("div",{"class":"search-result-item"}):
+            hike_name = hike_div.find(class_="listitem-title").find("span").text
+            highpoint_div = hike_div.find(class_="hike-highpoint hike-stat")
+            elevation = 0
+            if highpoint_div:
+                elevation = float(highpoint_div.find("span").text)
+            length_div = hike_div.find(class_="hike-length hike-stat")
+            length = 0
+            if length_div:
+                length = float(length_div.find("span").text.split()[0])
+            firp_hikes.append(Hike(hike_name, 0, 0, elevation, length, [], [], False))
+        return firp_hikes
 
-print(ScrapedIndex())
+
+
+
+print (ScrapedIndex().all_hikes())
