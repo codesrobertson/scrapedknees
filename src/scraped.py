dd4fab2648a37072cda0c3155a775aea09193d31
codesrobertson <arobertson335@gmail.com>
2019-10-15 21:58:17 -0700

    Added hike features classes.

--- src/scraped.py
+++ src/scraped.py
@@ -23,10 +23,51 @@ class ScrapedIndex:
         return firp_hikes
 
 class IndividualScraped:
-    def ind_hikes(self):
-        pass
-
     def __init__(self, page_link):
         page_link_response = requests.get(page_link)
+        index_content = BeautifulSoup(page_link_response.content, "html.parser")
+        lat,long = self.get_lat_long(index_content)
+        hike_name = self.get_hike_name(index_content)
 
-p = IndividualScraped ("https://www.wta.org/go-hiking/hikes/thorp-mountain-lookout-via-thorp-creek")
+    def get_lat_long(self,index_content):
+        lat_long = index_content.find(class_="latlong")
+        lat_long_spans = lat_long.find_all("span")
+        lat = float(lat_long_spans[0].text)
+        long = float(lat_long_spans[1].text)
+        return (lat,long)
+
+    def get_hike_name(self, index_content):
+        hike_name = index_content.find(class_="documentFirstHeading")
+        hike_name = hike_name.text
+        return (hike_name)
+
+    def get_hike_stats(self, index_content):
+        hike_stats = index_content.find(id="hike-stats")
+        stats_spans = hike_stats.find_all("span")
+        gain = float(stats_spans[0].text)
+        high_point = float(stats_spans[1].text)
+        return(gain, high_point)
+
+    def get_length(self, index_content):
+        length_stat = index_content.find(id="distance")
+        distance = length_stat.find_all("span")
+        if distance contains "one way", "one-way", "oneway":
+            float*2
+            return float
+        else
+            return(distance)
+        end
+
+    def properties(self, index_content):
+        properties_stats = index_content.find(id="hike-features")
+        properties_stats 
+
+
+
+
+
+
+
+
+
+IndividualScraped("https://www.wta.org/go-hiking/hikes/thorp-mountain-lookout-via-thorp-creek")
