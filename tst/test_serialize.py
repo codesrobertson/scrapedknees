d4b5e2f56a8aa7453d06d96df1370a10be589b32
Isaiah Mann <isaiahbmann@gmail.com>
2019-10-15 21:15:19 -0700

    Add deserialize TSV function

--- tst/test_serialize.py
+++ tst/test_serialize.py
@@ -1,10 +1,15 @@
 from hike import Hike
 from serialize import Serializer
 
+SINGLE_HIKE_FILE = "test_hikes.tsv"
+MULTI_HIKE_FILE = "test_hikes_multiple.tsv"
+
+
 def test_serialize_hike():
     hike = Hike("Nice Trail", 100.2221, 20.00, 200, 5, ["forest", "waterfall"], ["icon"], True)
-    tsv = Serializer("test_hikes.tsv")
+    tsv = Serializer(SINGLE_HIKE_FILE)
     tsv.serialize([hike])
+
 
 def test_serialize_multiple_hikes():
     hikes = [
@@ -13,8 +18,43 @@ def test_serialize_multiple_hikes():
         Hike("Chill Trail", 100.2221, 20.00, 200, 5, ["forest", "waterfall"], ["icon"], True),
         Hike("Decent Trail", 100.2221, 20.00, 200, 5, ["forest", "waterfall"], ["icon"], True)
     ]
-    tsv = Serializer("test_hikes_multiple.tsv")
+    tsv = Serializer(MULTI_HIKE_FILE)
     tsv.serialize(hikes)
+
+
+def test_deserialize_hike():
+    tsv = Serializer(SINGLE_HIKE_FILE)
+
+    hike = tsv.deserialize()[0]
+
+    assert hike.name == "Nice Trail"
+    assert hike.lat == 100.2221
+    assert hike.long == 20.00
+    assert hike.elevation == 200
+    assert hike.length == 5
+    assert hike.properties == ["forest", "waterfall"]
+    assert hike.permits == ["icon"]
+    assert hike.pets_allowed
+
+
+def test_deserialize_multiple_hikes():
+    tsv = Serializer(MULTI_HIKE_FILE)
+
+    all_hikes = tsv.deserialize()
+    hike_names = ["Nice Trail", "Cool Trail", "Chill Trail", "Decent Trail"]
+
+    for index, hike in enumerate(all_hikes):
+        assert hike.name == hike_names[index]
+        assert hike.lat == 100.2221
+        assert hike.long == 20.00
+        assert hike.elevation == 200
+        assert hike.length == 5
+        assert hike.properties == ["forest", "waterfall"]
+        assert hike.permits == ["icon"]
+        assert hike.pets_allowed
+
 
 test_serialize_hike()
 test_serialize_multiple_hikes()
+test_deserialize_hike()
+test_deserialize_multiple_hikes()
