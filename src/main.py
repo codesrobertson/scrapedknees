837085340425401185d3938beeb212da27ad05bb
codesrobertson <arobertson335@gmail.com>
2019-11-12 21:57:04 -0800

    Scraped individual hike pages from index page.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
 from serialize import Serializer
-from scraped import IndividualScraped
+from scraped import IndividualScraped, ScrapedIndex
 
 HIKE_FILE_PATH = "out/hikes.tsv"
 
 print("Scraping index page for hikes...")
 
-hike = IndividualScraped("https://www.wta.org/go-hiking/hikes/thorp-mountain-lookout-via-thorp-creek").hike
 hikes = []
-hikes.append(hike)
-hike = IndividualScraped("https://www.wta.org/go-hiking/hikes/mirror-lake-1").hike
-hikes.append(hike)
 
+for link in ScrapedIndex().all_hikes():
+    # TODO: properly handle this error
+    try:
+        hike = IndividualScraped(link).hike
+        hikes.append(hike)
+    except:
+        print("Unable to scrape hike from link " + link)
 
 print(f"Writing hikes to file: '{HIKE_FILE_PATH}'")
 Serializer(HIKE_FILE_PATH).serialize(hikes)
 print("Done!")
+
--- src/scraped.py
+++ src/scraped.py
@@ -10,16 +10,8 @@ class ScrapedIndex:
         index_content = BeautifulSoup (index_response.content, "html.parser")
         div = index_content.find(id="search-result-listing")
         for hike_div in div.findChildren ("div",{"class":"search-result-item"}):
-            hike_name = hike_div.find(class_="listitem-title").find("span").text
-            highpoint_div = hike_div.find(class_="hike-highpoint hike-stat")
-            elevation = 0
-            if highpoint_div:
-                elevation = float(highpoint_div.find("span").text)
-            length_div = hike_div.find(class_="hike-length hike-stat")
-            length = 0
-            if length_div:
-                length = float(length_div.find("span").text.split()[0])
-            firp_hikes.append(Hike(hike_name, 0, 0, elevation, length, [], [], False))
+            hike_link = hike_div.find(class_="listitem-title").attrs["href"]
+            firp_hikes.append(hike_link)
         return firp_hikes
 
 class IndividualScraped:
